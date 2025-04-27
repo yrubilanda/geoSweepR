8bda89c59e37254d3615097bdeadd88e8e5dc8ad
wompusjr <june.burke@utexas.edu>
2025-04-27 15:36:51 -0500

    june

--- WorkingScripts/June.R
+++ WorkingScripts/June.R
@@ -1,1 +1,15 @@
 # June's Scratchpad
+##libraries
+install.packages("sp")
+install.packages("gstat")
+library(tidyverse)
+library(sp)
+library(gstat)
+library(sf)
+##Step One - loading in the data
+d <- read_csv("Data/Clean_CrimeData_2024.csv", col_names = TRUE)
+head(d)
+class(d)
+
+#creating the variogram
+v <- variogram(log()~1, d)
